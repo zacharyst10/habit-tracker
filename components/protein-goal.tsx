e604a172fa521b59294c9ed015fb30f584a61dca
Zach Stout <stout.zachary@gmail.com>
2024-12-05 13:20:29 -0700

    third time is the charm

--- components/protein-goal.tsx
+++ components/protein-goal.tsx
@@ -1,35 +1,35 @@
-"use client";
+// "use client";
 
-export function ProteinGoalDisplay({ currentGoal }: { currentGoal: number }) {
-  //   const [newGoal, setNewGoal] = useState("");
-  //   return (
-  //     <div>
-  //       <div>Current Protein Goal: {currentGoal}g</div>
-  //       <div className="flex gap-2">
-  //         <input
-  //           type="number"
-  //           value={newGoal}
-  //           onChange={(e) => setNewGoal(e.target.value)}
-  //           className="border rounded p-2"
-  //           placeholder="Enter protein goal"
-  //         />
-  //         <Button
-  //           onClick={async () => {
-  //             if (newGoal) {
-  //               await createProteinGoalAction(Number(newGoal));
-  //               setNewGoal(""); // Clear input after setting
-  //             }
-  //           }}
-  //         >
-  //           Set Protein Goal
-  //         </Button>
-  //         <Button onClick={() => (
-  //             'use server'
-  //             await setupDatabase()
-  //         )}>
-  //             Set up database
-  //         </Button>
-  //       </div>
-  //     </div>
-  //   );
-}
+// export function ProteinGoalDisplay({ currentGoal }: { currentGoal: number }) {
+//     const [newGoal, setNewGoal] = useState("");
+//     return (
+//       <div>
+//         <div>Current Protein Goal: {currentGoal}g</div>
+//         <div className="flex gap-2">
+//           <input
+//             type="number"
+//             value={newGoal}
+//             onChange={(e) => setNewGoal(e.target.value)}
+//             className="border rounded p-2"
+//             placeholder="Enter protein goal"
+//           />
+//           <Button
+//             onClick={async () => {
+//               if (newGoal) {
+//                 await createProteinGoalAction(Number(newGoal));
+//                 setNewGoal(""); // Clear input after setting
+//               }
+//             }}
+//           >
+//             Set Protein Goal
+//           </Button>
+//           <Button onClick={() => (
+//               'use server'
+//               await setupDatabase()
+//           )}>
+//               Set up database
+//           </Button>
+//         </div>
+//       </div>
+//     );
+// }
