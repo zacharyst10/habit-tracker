4bc26dc91a8148ea6ba1d72d5f0a4fe1acb06c62
Zach Stout <stout.zachary@gmail.com>
2024-12-30 12:57:17 -0700

    delete pending status

--- components/delete-book.tsx
+++ components/delete-book.tsx
@@ -1,22 +1,24 @@
 "use client";
 import { deleteBook } from "@/actions/books";
+import { LoaderCircle } from "lucide-react";
+import { useFormStatus } from "react-dom";
+import { Button } from "./ui/button";
+
+function DeleteButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button variant={"ghost"} type="submit">
+      {pending ? <LoaderCircle className="h-4 w-4 animate-spin" /> : "Delete"}
+    </Button>
+  );
+}
 
 export function DeleteBook({ bookId }: { bookId: number }) {
-  const handleDelete = async () => {
-    try {
-      console.log("Deleting book:", bookId);
-      await deleteBook(bookId);
-      console.log("Book deleted");
-    } catch (error) {
-      console.error("Error deleting book:", error);
-    }
-  };
-
   return (
-    <div className="flex items-center justify-between">
-      <button onClick={handleDelete} className="w-full">
-        Delete
-      </button>
+    <div className="text-center">
+      <form action={() => deleteBook(bookId)}>
+        <DeleteButton />
+      </form>
     </div>
   );
 }
